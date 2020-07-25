65a93134c40f1fa803e8d73a607ed095df1b5a68
Shiiisuiii <kolesnik1310.dev@gmail.com>
2020-07-25 19:36:11 +0300

    Split Card component and add code more readable

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Card from './components/Card';
-import './index.css';
 import * as serviceWorker from './serviceWorker';
+import CardArray from './components/CardsList';
 import tachyon from 'tachyons';
 import { robots }  from './db/robots'
 
 ReactDOM.render(
   <div>
-     <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-     <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-     <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+     <CardArray robots={robots}/>
   </div>,
   document.getElementById('root')
 );
